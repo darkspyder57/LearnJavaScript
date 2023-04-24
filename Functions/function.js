edd8349506135c5bb395fe7a0ef84b8dd6c68b1c
Mohnish <mohnishkumar57@gmail.com>
2023-04-24 23:16:35 +0530

    deleted

--- Functions/function.js
+++
@@ -1,7 +0,0 @@
-function cookMaggi(maggie , water , pan){
-    console.log(" Your maggie will be ready in " +
-    maggie * 2 + " minutes " + " and ingredients used are:- "+ 
-    maggie + " cups of water "+ " using " + pan + " pan ")
-}
-
-cookMaggi(4, 8, 1);
