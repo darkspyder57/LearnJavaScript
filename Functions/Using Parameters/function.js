6a53ad6f12471e497362d2293d1ee8e5ecfd0a5d
Mohnish <mohnishkumar57@gmail.com>
2023-04-24 23:12:23 +0530

    function using parameters

---
+++ Functions/Using Parameters/function.js
@@ -0,0 +1,7 @@
+function cookMaggi(maggie , water , pan){
+    console.log(" Your maggie will be ready in " +
+    maggie * 2 + " minutes " + " and ingredients used are:- "+ 
+    maggie + " cups of water "+ " using " + pan + " pan ")
+}
+
+cookMaggi(4, 8, 1);
