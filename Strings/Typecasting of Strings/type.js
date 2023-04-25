a72ad5d90c6b39159d5bfcc678df755faef21526
Mohnish <mohnishkumar57@gmail.com>
2023-04-25 14:56:14 +0530

    Type-Casting a String

---
+++ Strings/Typecasting of Strings/type.js
@@ -0,0 +1,6 @@
+let strType = "11";
+console.log(strType);
+var String = typeof(strType);
+console.log(String); // converted the type of Integer to String
+var num = Number(strType);
+console.log(num);  //converted the type of String to Integer
