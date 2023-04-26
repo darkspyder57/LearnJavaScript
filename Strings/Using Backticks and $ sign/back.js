7fd1879afa7aaaa4f895bb92a248c67ff69c3cc8
Mohnish <mohnishkumar57@gmail.com>
2023-04-26 21:46:31 +0530

    Strings using backticks

---
+++ Strings/Using Backticks and $ sign/back.js
@@ -0,0 +1,5 @@
+let hello = "Hello this is your friendly neighboring coder DarkSpyder";
+let welcome = "Welcome to my Github Profile";
+let number = 123;
+let displayMsg = `${hello+" and "+welcome} Your score is ${number}`;
+console.log(displayMsg);
